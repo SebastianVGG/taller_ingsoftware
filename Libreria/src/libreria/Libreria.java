975cff8621269c8aa8d62b7a5e1dddbe61ce528d
Sebastian <Sebastian@DESKTOP-AVS2D8F>
2021-01-18 14:22:17 -0600

    Login en marcha

--- Libreria/src/libreria/Libreria.java
+++ Libreria/src/libreria/Libreria.java
@@ -12,19 +12,8 @@ import login.Main_login;
 public class Libreria extends javax.swing.JFrame{
  
     public static void main(String[] args) {
-        
-        new Main_login().setVisible(true);
-        JFrame frame = new JFrame();
-        frame.setResizable(false);
-        frame.setUndecorated(true);
-        
-        
         Main_login login = new Main_login();
-        frame.add(login);
-        frame.pack();
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true); 
-        
+        login.setVisible(true);
     }
     
 }
